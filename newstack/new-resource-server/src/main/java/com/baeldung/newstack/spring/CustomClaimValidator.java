6849816cd243294be024516b3cb0cd5f5efbac0d
Miguel Vaamonde <mvaamfer@gmail.com>
2020-03-25 20:41:22 +0100

    Some changes to use OAuth2TokenValidator

--- newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/CustomClaimValidator.java
+++ newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/CustomClaimValidator.java
@@ -1,10 +1,15 @@
 package com.baeldung.newstack.spring;
 
 import org.apache.commons.validator.routines.EmailValidator;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,32 +17,48 @@ import java.util.Map;
  * @project new-stack
  */
 @Component
-public class CustomClaimValidator {
+public class CustomClaimValidator implements OAuth2TokenValidator<Jwt> {
 
     private final static String BAELDUNG_DOMAIN = "baeldung.com";
     private final static String USER_NAME_CLAIM = "preferred_username";
+    private List<OAuth2Error> errors;
 
-    public void verify(Map<String, Object> claims) throws InvalidTokenException {
+
+    @Override
+    public OAuth2TokenValidatorResult validate(Jwt jwt) {
+        this.errors = new ArrayList<>();
+        Map<String, Object> claims = jwt.getClaims();
+        if(this.verifyClaims(claims)){
+            return OAuth2TokenValidatorResult.success();
+        }
+        return OAuth2TokenValidatorResult.failure(this.errors);
+    }
+
+    private boolean verifyClaims(Map<String, Object> claims){
         System.out.println(claims);
         final String username = (String) claims.get(USER_NAME_CLAIM);
-        checkUserLength(username);
-        checkUserDomain(username);
+        return isValidUserLength(username) && isValidUserDomain(username);
     }
 
-    private void checkUserLength(String username){
-        if ( StringUtils.isEmpty(username)) {
-            throw new InvalidTokenException(USER_NAME_CLAIM.concat(" claim is empty"));
-        }
+    private boolean isValidUserLength(String username){
+         if(StringUtils.isEmpty(username)){
+             errors.add(new OAuth2Error("invalid_token", "User name length is not valid", null));
+             return false;
+         }
+         return true;
     }
 
-    private void checkUserDomain(String username){
+    private boolean isValidUserDomain(String username){
         if(!EmailValidator.getInstance().isValid(username)){
-            throw new InvalidTokenException(USER_NAME_CLAIM.concat(" claim is not a valid email"));
+            errors.add(new OAuth2Error("invalid_token", "The user name it is not a valid email", null));
+            return false;
         }else{
             String domain = username.substring(username .indexOf("@") + 1);
             if(!domain.equals(BAELDUNG_DOMAIN)){
-                throw new InvalidTokenException(USER_NAME_CLAIM.concat(" claim does not belong to baeldung domain"));
+                errors.add(new OAuth2Error("invalid_token", "The user name does not belong to the required domain", null));
+                return false;
             }
+            return true;
         }
     }
 }
--- newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/SecurityConfig.java
+++ newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/SecurityConfig.java
@@ -1,22 +1,32 @@
 package com.baeldung.newstack.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-
-import java.util.Collection;
-import java.util.Map;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private CustomClaimValidator customClaimValidator;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        OAuth2TokenValidator<Jwt> customClaimValidator = new CustomClaimValidator();
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(this.issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, customClaimValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
+
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
@@ -29,17 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(new JwtAuthenticationConverter() {
-                    @Override
-                    protected Collection<GrantedAuthority> extractAuthorities(final Jwt jwt)
-                    {
-                        Collection<GrantedAuthority> authorities = super.extractAuthorities(jwt);
-                        Map<String, Object> claims = jwt.getClaims();
-                        customClaimValidator.verify(claims);
-                        return authorities;
-                    }
-                });
+                .jwt();
     }//@formatter:on
 
 }
